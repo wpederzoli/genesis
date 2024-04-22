95ee42cd8cb21f586bf565d72d524322c62935ce
William Pederzoli <wpederzoli@gmail.com>
2024-04-22 08:30:52 +0200

    Session 05

--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -1,189 +1,144 @@
 use log::error;
 use wgpu::{
     Device, DeviceDescriptor, Instance, InstanceDescriptor, Queue, RequestAdapterOptions, Surface,
+    SurfaceConfiguration, TextureUsages,
 };
 use winit::{
     event::{Event, KeyEvent, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
-    keyboard::{KeyCode, PhysicalKey},
+    keyboard::{self, KeyCode, PhysicalKey},
     window::{Window, WindowBuilder},
 };
 
-pub struct Engine {
-    event_loop: Option<EventLoop<()>>,
+pub struct Engine<'a> {
+    event_loop: EventLoop<()>,
     device: Option<Device>,
     queue: Option<Queue>,
-    surface: Option<Surface<'static>>,
+    surface: Option<Surface<'a>>,
+    config: Option<SurfaceConfiguration>,
+    window: Window,
 }
 
-impl Engine {
+impl<'a> Engine<'a> {
     pub fn new(title: &str) -> Self {
-        let mut engine = Engine::default();
-
-        match EventLoop::new() {
-            Ok(el) => engine.event_loop = Some(el),
-            Err(e) => {
-                error!("Failed to create event loop: {}", e);
-                return Engine::default();
-            }
-        };
-
-        let window: Window;
-        match WindowBuilder::new()
+        let event_loop = EventLoop::new().unwrap();
+        let window = WindowBuilder::new()
             .with_title(title)
-            .build(&engine.event_loop.as_ref().unwrap())
-        {
-            Ok(win) => window = win,
-            Err(e) => {
-                error!("Failed to create window: {}", e);
-                return Engine::default();
-            }
-        }
-
-        engine
-            .event_loop
-            .as_ref()
-            .unwrap()
-            .set_control_flow(ControlFlow::Poll);
-
-        let size = window.inner_size();
-        let instance = Instance::new(InstanceDescriptor::default());
-
-        match instance.create_surface(window) {
-            Ok(surface) => engine.surface = Some(surface),
-            Err(e) => {
-                error!("Failed to create surface: {}", e);
-                return Engine::default();
-            }
-        }
-
-        let adapter = pollster::block_on(instance.request_adapter(&RequestAdapterOptions {
-            power_preference: wgpu::PowerPreference::default(),
-            force_fallback_adapter: false,
-            compatible_surface: Some(&engine.surface.as_ref().unwrap()),
-        }))
-        .unwrap();
-
-        match pollster::block_on(adapter.request_device(
-            &DeviceDescriptor {
-                label: None,
-                required_features: wgpu::Features::empty(),
-                required_limits:
-                    wgpu::Limits::downlevel_webgl2_defaults().using_resolution(adapter.limits()),
-            },
-            None,
-        )) {
-            Ok((device, queue)) => {
-                engine.device = Some(device);
-                engine.queue = Some(queue);
-            }
-            Err(e) => {
-                error!("Unable to create device and queue: {}", e);
-                return Engine::default();
-            }
-        }
-
-        let config = engine
-            .surface
-            .as_ref()
-            .unwrap()
-            .get_default_config(&adapter, size.width, size.height)
+            .build(&event_loop)
             .unwrap();
-        engine
-            .surface
-            .as_ref()
-            .unwrap()
-            .configure(&engine.device.as_ref().unwrap(), &config);
 
-        engine
-    }
-
-    pub fn run(self) {
-        match self.event_loop {
-            Some(ev) => {
-                let Engine {
-                    queue,
-                    surface,
-                    device,
-                    ..
-                } = self;
-                let surface = surface.unwrap();
-                let queue = queue.unwrap();
-                let device = device.unwrap();
-
-                ev.run(move |event, target| match event {
-                    Event::WindowEvent {
-                        event: WindowEvent::CloseRequested,
-                        ..
-                    } => target.exit(),
-                    Event::WindowEvent {
-                        event:
-                            WindowEvent::KeyboardInput {
-                                event:
-                                    KeyEvent {
-                                        physical_key: PhysicalKey::Code(KeyCode::Escape),
-                                        ..
-                                    },
-                                ..
-                            },
-                        ..
-                    } => target.exit(),
-                    Event::WindowEvent {
-                        event: WindowEvent::RedrawRequested,
-                        ..
-                    } => {
-                        let frame = surface
-                            .get_current_texture()
-                            .expect("Failed to get texture");
-                        let view = frame
-                            .texture
-                            .create_view(&wgpu::TextureViewDescriptor::default());
-                        let mut encoder =
-                            device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
-                                label: None,
-                            });
-
-                        encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-                            label: None,
-                            color_attachments: &[Some(wgpu::RenderPassColorAttachment {
-                                view: &view,
-                                resolve_target: None,
-                                ops: wgpu::Operations {
-                                    load: wgpu::LoadOp::Clear(wgpu::Color {
-                                        r: 0.3,
-                                        g: 0.5,
-                                        b: 0.4,
-                                        a: 1.0,
-                                    }),
-                                    store: wgpu::StoreOp::Store,
-                                },
-                            })],
-                            depth_stencil_attachment: None,
-                            timestamp_writes: None,
-                            occlusion_query_set: None,
-                        });
-
-                        queue.submit(Some(encoder.finish()));
-                        frame.present();
-                    }
-                    _ => (),
-                })
-                .unwrap();
-            }
-            None => {
-                error!("Event loop is not set!");
-            }
-        }
-    }
-}
-
-impl Default for Engine {
-    fn default() -> Self {
         Engine {
-            event_loop: None,
+            event_loop,
+            window,
             device: None,
             queue: None,
             surface: None,
+            config: None,
         }
+    }
+
+    fn wgpu_setup(&'a mut self) {
+        let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
+            backends: wgpu::Backends::all(),
+            ..Default::default()
+        });
+
+        let surface = instance.create_surface(&self.window).unwrap();
+
+        let adapter = pollster::block_on(instance.request_adapter(&wgpu::RequestAdapterOptions {
+            power_preference: wgpu::PowerPreference::default(),
+            compatible_surface: Some(&surface),
+            force_fallback_adapter: false,
+        }))
+        .unwrap();
+
+        let (device, queue) = pollster::block_on(adapter.request_device(
+            &DeviceDescriptor {
+                required_features: wgpu::Features::empty(),
+                required_limits: wgpu::Limits::downlevel_webgl2_defaults(),
+                label: None,
+            },
+            None,
+        ))
+        .unwrap();
+
+        let size = self.window.inner_size();
+
+        let config = surface
+            .get_default_config(&adapter, size.width, size.height)
+            .unwrap();
+
+        surface.configure(&device, &config);
+
+        self.surface = Some(surface);
+        self.config = Some(config);
+        self.device = Some(device);
+        self.queue = Some(queue);
+    }
+
+    pub fn run(mut self) {
+        self.wgpu_setup();
+
+        let surface = self.surface.unwrap();
+        let device = self.device.unwrap();
+        let queue = self.queue.unwrap();
+
+        self.event_loop
+            .run(move |event, target| match event {
+                Event::WindowEvent {
+                    event: WindowEvent::CloseRequested,
+                    ..
+                } => target.exit(),
+                Event::WindowEvent {
+                    event:
+                        WindowEvent::KeyboardInput {
+                            event:
+                                KeyEvent {
+                                    physical_key: keyboard::PhysicalKey::Code(KeyCode::Escape),
+                                    ..
+                                },
+                            ..
+                        },
+                    ..
+                } => target.exit(),
+                Event::WindowEvent {
+                    event: WindowEvent::RedrawRequested,
+                    ..
+                } => {
+                    let frame = surface
+                        .get_current_texture()
+                        .expect("Failed to get texture");
+                    let view = frame
+                        .texture
+                        .create_view(&wgpu::TextureViewDescriptor::default());
+                    let mut encoder = device
+                        .create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
+
+                    encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                        label: None,
+                        color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                            view: &view,
+                            resolve_target: None,
+                            ops: wgpu::Operations {
+                                load: wgpu::LoadOp::Clear(wgpu::Color {
+                                    r: 0.3,
+                                    g: 0.5,
+                                    b: 0.4,
+                                    a: 1.0,
+                                }),
+                                store: wgpu::StoreOp::Store,
+                            },
+                        })],
+                        depth_stencil_attachment: None,
+                        timestamp_writes: None,
+                        occlusion_query_set: None,
+                    });
+
+                    queue.submit(Some(encoder.finish()));
+                    frame.present();
+                }
+                _ => (),
+            })
+            .unwrap();
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,10 @@ use engine::Engine;
 
 mod engine;
 
-//TODO: esc key
 //TODO: resize
 //TODO: refactor
 //TODO: clear resources in event loop run
+//TODO: fix lifetimes (prob use for<'a> syntax)
 
 fn main() {
     Engine::new("Genesis").run();
